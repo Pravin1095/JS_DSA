b7cb0bdb10b60d6ffa6f4f6847d2be66b7815823
STAPLESAMS\AruPr001 <Pravin.Arun@staples.com>
2024-07-02 19:57:18 +0530

    SumZeroPairs new solution

--- ProblemSolvingPattern/Multiple Pointers/SumZeroPairs.js
+++ ProblemSolvingPattern/Multiple Pointers/SumZeroPairs.js
@@ -26,4 +26,28 @@ while(startPoint<endPoint){
 return undefined
 }
 
+const EffectiveSol2=(arr)=>{
+    let lowIndex=0
+    let highIndex=arr.length-1
+    let hasNeg=arr.some(num=>num<0)
+    console.log('index out', lowIndex, highIndex)
+    if(!hasNeg){
+        return 'No Pairs'
+    }
+    else{
+        while(lowIndex<highIndex){
+            console.log('index',lowIndex,highIndex)
+        if(arr[lowIndex]+arr[highIndex]>0){
+            highIndex-=1
+        }
+        else if(arr[lowIndex]+arr[highIndex]<0){
+            lowIndex+=1
+        }
+        else{
+            return [arr[lowIndex], arr[highIndex]]
+        }
+    }
+    }
+}
+
 console.log(EffectiveSol([-4,-3,-2,-1,0,1,5]))
