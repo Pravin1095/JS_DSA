4a70cfe4ea7c8f5fb085ad429c802ee85f889c1d
Pravin A <a.pravin3210@gmail.com>
2025-04-06 23:51:28 +0530

    Random Programs

---
+++ Random programs/ReverseString.js
@@ -0,0 +1,43 @@
+
+class RandomPrograms{
+//     constructor(val){
+// this.val = val
+//     }
+    reverse = (s)=>{
+        let res =''
+        let i =1
+    if(s==''){
+        return ''
+    }
+    while(i<=s.length){
+    res+=s[s.length-i]
+    i+=1
+    }
+    return res
+    }
+
+   vowelCount = (s)=>{
+    let string = s.toLowerCase()
+let map =new Map();
+map.set('a',0)
+map.set('e',0)
+map.set('i',0)
+map.set('o',0)
+map.set('u',0)
+let count = 0;
+for(let i=0;i<s.length;i++){
+    if(map.has(s[i])){
+        count+=1
+    }
+    else{
+        continue
+    }
+}
+return count
+   }
+
+}
+
+obj1 = new RandomPrograms()
+console.log(obj1.reverse('pravin'))
+console.log(obj1.vowelCount('pravineee'))
